61d64d32a5cc6303f15225ade71f8a43bb9b5f32
jrhender <johnhenderson45@gmail.com>
2017-08-17 18:40:35 -0700

    Final merge with remote

--- Models/DbConnectionConfig.cs
+++ Models/DbConnectionConfig.cs
@@ -2,12 +2,6 @@ namespace AskToniApi.Models
 {
     public class DbConnectionConfig
     {
-<<<<<<< HEAD
         public string mLabConnectStr { get; set; }
-=======
-        public string AskToniDbUserPassword { get; set; }
-        public string AskToniDbUserName { get; set; }
-        public string AskToniDbName { get; set; }
->>>>>>> origin/dbsettings
     }
 }
