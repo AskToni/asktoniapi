eadaaa41c2273fd6e352453b19e780805fd30d57
jrhender <johnhenderson45@gmail.com>
2017-08-22 09:36:13 -0700

    add RestaurantID to model and moved gitignore file to root

--- src/WebAPI/Models/Restaurant.cs
+++ src/WebAPI/Models/Restaurant.cs
@@ -8,22 +8,34 @@ namespace AskToniApi.Models
     {
         [BsonId]
         public ObjectId Id { get; set; }
+
+        [BsonElement("RestaurantId")]
+        public string RestaurantId { get; set;}
+        
         [BsonElement("RestaurantName")]
         public string RestaurantName { get; set;}
+        
         [BsonElement("ReviewCount")]
         public int ReviewCount { get; set;}
+        
         [BsonElement("Rating")]
         public double Rating { get; set;}
+        
         [BsonElement("Price")]
         public string Price { get; set;}
+        
         [BsonElement("Address")]
         public string Address {get; set;}
+        
         [BsonElement("City")]
         public string City {get; set;}
+        
         [BsonElement("ZipCode")]
         public string ZipCode {get; set;}
+        
         [BsonElement("Phone")]
         public string Phone { get; set;}
+        
         [BsonElement("Categories")]
         public string[] Categories {get; set;}
     }
