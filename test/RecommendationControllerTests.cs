3ff3e32a06f0a20f7c2d0d7af4535e13490199d2
jrhender <johnhenderson45@gmail.com>
2017-08-24 19:11:53 -0700

    created XUnit proj (again)

---
+++ test/RecommendationControllerTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using AskToniApi.Controllers;
+
+namespace AskToniApi.UnitTests.Controllers
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
