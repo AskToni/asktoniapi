fc8cc715984c675c6e5307f9fbc985d132302172
jrhender <johnhenderson45@gmail.com>
2017-09-06 21:23:44 -0700

    added solution file, split into iteg and unit tests

--- docs/testingApproach.md
+++ docs/testingApproach.md
@@ -2,9 +2,15 @@ AskToni API Testing Approach
 ============================
 Test Projects and File Structure
 --------------------------------
+Integration Tests:
+UnitTests:
+TestsCore: This contains useful classes for testing
+* TestingObject.cs : A wrapper class for mocks with dependancies, see [1]
+
 References
 ----------
-The [main guide][1] initially followed.
-Also reference this official documentation
+For unit testing, this is the [main guide][1] initially followed.
+For integration testing (though is also references unit testing), this [microsoft docs][2] article was referenced
 
-[1]: https://rushfive.github.io/Start-Unit-Testing-with-xUnit-Moq/   "main guide"
+[1]: https://rushfive.github.io/Start-Unit-Testing-with-xUnit-Moq/   "unit testing with xUnit & Moq"
+[2]: https://docs.microsoft.com/en-us/aspnet/core/mvc/controllers/testing "MS Docs on controller testing"
---
+++ test/UnitTests/Controllers/RecommendationControllerUnitTests.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Moq;
+using AskToniApi.Controllers;
+using AskToniApi.Models;
+using TestsCore;
+
+namespace AskToniApi.UnitTests.Controllers
+{
+    public class RecommendationControllerUnitTests
+    {
+        [Fact]
+        public void Get_Success_ReturnsCorrectResult()
+        {
+            var mockRecommendationRepository = new Mock<IRecommendationRepository>();
+            var restaurantCollection = new List<Restaurant>();
+            restaurantCollection.Add(new Restaurant() 
+                                        { 
+                                            RestaurantName = "testName",
+                                            ReviewCount = 2
+                                        }
+                                    );
+            mockRecommendationRepository.Setup(d => d.GetAllRestaurants()).ReturnsAsync(restaurantCollection);
+
+            var recommendationController = new RecommendationController(mockRecommendationRepository.Object);
+            var result = recommendationController.Get();
+            Assert.Equal("testName",result.Result.First().RestaurantName);
+        }
+    }
+}
